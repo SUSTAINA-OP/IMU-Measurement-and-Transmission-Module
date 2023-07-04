7065241254c4b3b41145f21f9d03d668fe04956d
MasatoKubotera <masatokubotera06@yahoo.co.jp>
2023-07-04 16:57:35 +0200

    Allowed the master cpu to perform the reset

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -29,52 +29,67 @@ void loop()
 {
   if (Serial.available())
   {
-    Serial.read();
-
-    // read the sensor
-    IMU.getAGT();
-
-    float _accX = IMU.accX();
-    float _accY = IMU.accY();
-    float _accZ = IMU.accZ();
-    float _gyrX = IMU.gyrX();
-    float _gyrY = IMU.gyrY();
-    float _gyrZ = IMU.gyrZ();
-
-    // convert 6 float variables to byte array
-    byte *ptr = NULL;
-    byte data[sizeof(float) * 6];
-    for (int data_itr = 0; data_itr < 6; data_itr++)
+    byte received_char = Serial.read();
+    if (received_char == 'r' || received_char == 'R')
     {
-      switch (data_itr)
+      // start communication with IMU
+      int status = IMU.begin();
+      if (status < 0)
       {
-      case 0:
-        ptr = (byte *)&_accX;
-        break;
-      case 1:
-        ptr = (byte *)&_accY;
-        break;
-      case 2:
-        ptr = (byte *)&_accZ;
-        break;
-      case 3:
-        ptr = (byte *)&_gyrX;
-        break;
-      case 4:
-        ptr = (byte *)&_gyrY;
-        break;
-      case 5:
-        ptr = (byte *)&_gyrZ;
-        break;
+        while (1)
+        {
+        }
       }
-      for (int ptr_itr = 0; ptr_itr < sizeof(float); ptr_itr++)
-      {
-        data[data_itr * sizeof(float) + ptr_itr] = *ptr;
-        ptr++;
-      }
+      
+      Serial.write("c", sizeof(byte));
     }
+    else
+    {
+      // read the sensor
+      IMU.getAGT();
 
-    // send byte array via serial communication
-    Serial.write(data, sizeof(data));
+      float _accX = IMU.accX();
+      float _accY = IMU.accY();
+      float _accZ = IMU.accZ();
+      float _gyrX = IMU.gyrX();
+      float _gyrY = IMU.gyrY();
+      float _gyrZ = IMU.gyrZ();
+
+      // convert 6 float variables to byte array
+      byte *ptr = NULL;
+      byte data[sizeof(float) * 6];
+      for (int data_itr = 0; data_itr < 6; data_itr++)
+      {
+        switch (data_itr)
+        {
+          case 0:
+            ptr = (byte *)&_accX;
+            break;
+          case 1:
+            ptr = (byte *)&_accY;
+            break;
+          case 2:
+            ptr = (byte *)&_accZ;
+            break;
+          case 3:
+            ptr = (byte *)&_gyrX;
+            break;
+          case 4:
+            ptr = (byte *)&_gyrY;
+            break;
+          case 5:
+            ptr = (byte *)&_gyrZ;
+            break;
+        }
+        for (int ptr_itr = 0; ptr_itr < sizeof(float); ptr_itr++)
+        {
+          data[data_itr * sizeof(float) + ptr_itr] = *ptr;
+          ptr++;
+        }
+      }
+
+      // send byte array via serial communication
+      Serial.write(data, sizeof(data));
+    }
   }
 }
